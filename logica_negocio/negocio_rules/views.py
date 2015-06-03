8915a3bf12b710dd57f93324b9e8932a52f37e20
andressmateo <andressmateo@hotmail.com>
2015-06-03 18:51:12 -0500

    Adding stuff

--- logica_negocio/negocio_rules/views.py
+++ logica_negocio/negocio_rules/views.py
@@ -45,22 +45,3 @@ def obtener_conferencias():
         }
         result_json.append(cu)
     return jsonify(conferencias=result_json)
-
-
-'''
-@app.route("/add_conferencia", methods=['POST', 'GET'])
-def add_conferencia():
-    data.add_conferencia(request.args.get("nombre"),request.args.get("descripcion"),request.args.get("fecha"),
-                         request.args.get("precio"))
-    return
-
-@app.route("/add_evento", methods=['POST', 'GET'])
-def add_evento():
-    data.add_evento(request.args.get("nombre"),request.args.get("tipo"))
-    return
-
-@app.route("/add_silla", methods=['POST', 'GET'])
-def add_silla():
-    data.add_silla(request.args.get("tipo"))
-    return
-'''
